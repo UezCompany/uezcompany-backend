1d079d866e7b6bbd9814c4cc0d92e5f41f55cc7b
Renato Gomes do Nascimento <110681726+RENATO-GDN@users.noreply.github.com>
2023-08-04 14:45:01 -0300

    Rotas Get = cliente

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (connection) => {
-  // Rota de exemplo
-  router.get('/', (req, res) => {
-    connection.query('SELECT * FROM tabela', (error, results, fields) => {
+  // Rota /clientes - GET
+  router.get('/clientes', (req, res) => {
+    connection.query('SELECT * FROM cliente', (error, results, fields) => {
       if (error) {
         console.error('Erro ao executar consulta: ' + error.stack);
         return res.status(500).json({ message: 'Erro ao executar consulta' });
@@ -12,6 +12,29 @@ module.exports = (connection) => {
 
       res.json(results);
     });
+  });
+  router.get('/clientes/:id', (req, res) => {
+    connection.query('SELECT * FROM cliente WHERE idCliente = ?', [req.params.id], (error, results, fields) => {
+      if (error) {
+        console.error('Erro ao executar consulta: ' + error.stack);
+        return res.status(500).json({ message: 'Erro ao executar consulta' });
+      }
+
+      res.json(results);
+    })
+  });
+
+
+  // Rota /clientes - POST
+  router.post('/clientes', (req, res) => {
+    connection.query('INSERT INTO cliente SET ?', req.body, (error, results, fields) => {
+      if (error) {
+        console.error('Erro ao executar consulta: ' + error.stack);
+        return res.status(500).json({ message: 'Erro ao executar consulta' });
+      }
+
+      res.json(results);
+    })
   });
 
   return router;
