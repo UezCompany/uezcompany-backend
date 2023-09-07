951b1c197430f04ba74c678eb6cbd146dadbdbd0
lordaval <lordaval1117@gmail.com>
2023-09-06 22:34:20 -0300

    Set correct host do deploy

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 
 const port = process.env.PORT || 3333;
-const host = process.env.HOST || 'localhost';
+const host = process.env.HOST || '0.0.0.0';
 
 server.listen(port, host, () => {
   console.log(`Servidor iniciado em http://${host}:${port}`);
