c4cf8f2db0b73eec3356ea0d8cad8935079a5734
Renato Gomes do Nascimento <110681726+RENATO-GDN@users.noreply.github.com>
2023-11-23 12:40:11 -0300

    ✌️

--- src/models/notificationsModel.js
+++ src/models/notificationsModel.js
@@ -3,28 +3,20 @@ const Notifications = require("./Schemas/Notifications")
 
 const notificationsModel = {
     getNotifications: async (idUser) => {
-
         const notifications = await Notifications.find({ _idUser: idUser })
         return notifications
-
     },
     pushNotifications: async (idUser, content, type) => {
-
         const push = await Notifications.create({ type: type, content: content, _idUser: idUser })
         return push
-
     },
     markAsRead: async (idNotification) => {
-        console.log(idNotification)
-
         const push = await Notifications.updateOne({_id: idNotification}, { readed: true })
         return push
     },
     deleteNotification: async (idNotifications) => {
-
         const deletedNotification = await Notifications.deleteOne({ _id: idNotifications })
         return deletedNotification
-
     }
 
 }
