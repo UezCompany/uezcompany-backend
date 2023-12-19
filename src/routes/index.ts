e56b5b150f3bdb198227e96404e1748cf37c1021
lordaval <lordaval1117@gmail.com>
2023-12-19 01:16:58 -0300

    chore: add inital setup

---
+++ src/routes/index.ts
@@ -0,0 +1,25 @@
+import { Router } from "express"
+
+// import clientesRoutes from "./clientesRoutes"
+// import uzersRoutes from "./uzersRoutes"
+// import funcionariosRoutes from "./funcionariosRoutes"
+// import servicosRoutes from "./servicosRoutes"
+// import authRoutes from "./authRoutes"
+// import userRoutes from "./userRoutes"
+// import pedidosRoutes from "./pedidosRoutes"
+// import chatRoutes from "./chatRoutes"
+// import notificationsRoutes from "./notificationsRoutes"
+
+const router = Router()
+
+// router.use(clientesRoutes)
+// router.use(uzersRoutes)
+// router.use(funcionariosRoutes)
+// router.use(servicosRoutes)
+// router.use(authRoutes)
+// router.use(userRoutes)
+// router.use(pedidosRoutes)
+// router.use(chatRoutes)
+// router.use(notificationsRoutes)
+
+export default router
