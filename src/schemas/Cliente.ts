76bf9af77824913d29e10259540d414ef24b4124
lordaval <lordaval1117@gmail.com>
2023-12-20 02:05:25 -0300

    feat: add cliente mvc

---
+++ src/schemas/Cliente.ts
@@ -0,0 +1,106 @@
+import { Schema, model } from "mongoose"
+import ICliente from "../@types/Cliente"
+
+const clienteSchema = new Schema<ICliente>(
+  {
+    nome: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      default: null,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    CPF: {
+      type: String,
+      required: true,
+    },
+    RG: {
+      type: String,
+      default: null,
+    },
+    senha: {
+      type: String,
+      required: true,
+    },
+    situacao: {
+      type: String,
+      default: "Ativo",
+    },
+    motivoBloqueio: {
+      type: String,
+      default: null,
+    },
+    CEP: {
+      type: String,
+      default: null,
+    },
+    endereco: {
+      type: Object,
+      default: {
+        logradouro: null,
+        numero: null,
+        complemento: null,
+        bairro: null,
+        cidade: null,
+        estado: null,
+      },
+    },
+    dataNascimento: {
+      type: Date,
+      default: "",
+    },
+    dataCadastro: {
+      type: Date,
+      default: new Date(),
+    },
+    telefone: {
+      type: String,
+      default: null,
+    },
+    userType: {
+      type: String,
+      default: "cliente",
+    },
+    aprovacao: {
+      type: Boolean,
+      default: false,
+    },
+    avaliacao: {
+      type: Number,
+      default: 0,
+    },
+    reprovacao: {
+      type: Boolean,
+      default: false,
+    },
+    quantidadePedidos: {
+      type: Number,
+      default: 0,
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://i.pinimg.com/280x280_RS/53/66/5d/53665d574976a6b66d283d7e3323bab9.jpg",
+    },
+    chats: [
+      {
+        _idChat: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    versionKey: "__versionOfSchema__",
+  },
+)
+
+const Cliente = model("Cliente", clienteSchema)
+
+export default Cliente
