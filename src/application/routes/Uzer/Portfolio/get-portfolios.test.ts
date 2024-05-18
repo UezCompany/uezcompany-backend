44cef383221220cc1cc1eebd27c07fc7da561db7
Danilo <htpps.sw@gmail.com>
2024-05-18 10:45:53 -0300

    feet: create uzers route test

---
+++ src/application/routes/Uzer/Portfolio/get-portfolios.test.ts
@@ -0,0 +1,28 @@
+import app from "@/application/server";
+import { expect, test } from "vitest";
+
+
+test('GET /portifolio/:slug', async () => {
+     const publicURL = '' // A url padrao para requisição
+     const slug = '' // A slug do usuario
+
+     const responseCookie = await app.inject({
+          method: 'POST',
+          url: `${publicURL}/login`,
+          payload: {
+               email: '',
+               senha: ''
+          }
+     })
+
+     const response = await app.inject({
+          method: 'GET',
+          headers: {
+               "set-cookie": responseCookie.headers.cookie // talvez seja necessario mudar o 'cookie' para setCookie
+          },
+          url: `${publicURL}/portifolio/${slug}`
+     })
+
+     expect(response.statusCode).toBe(200) 
+     // Fazer testes do retorno payload 
+})
---
+++ src/application/routes/Uzer/get-uzers.test.ts
@@ -0,0 +1,29 @@
+import app from "@/application/server";
+import { expect, test } from "vitest";
+
+
+
+test('GET /uzers', async () => {
+     const publicURL = '' // A url padrao para requisição
+     
+     const responseCookie = await app.inject({
+          method: 'POST',
+          url: `${publicURL}/login`,
+          payload: {
+               email: '',
+               senha: ''
+          }
+     })
+
+
+     const response = await app.inject({
+          method: 'GET',
+          headers: {
+               "set-cookie": responseCookie.headers.cookie // talvez seja necessario mudar o 'cookie' para setCookie
+          },
+          url: `${publicURL}/uzers`
+     })
+
+     expect(response.statusCode).toBe(200)
+     // Fazer testes do retorno payload 
+})
