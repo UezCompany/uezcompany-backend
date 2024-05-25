40b12a88bfc6d2a411c6c28bd1081bda1b71314d
Danilo <htpps.sw@gmail.com>
2024-05-25 01:24:53 -0300

    feet: create client route test

---
+++ test/Routes/cliente.test.ts
@@ -0,0 +1,46 @@
+import app from "@/application/server"
+import { describe, expect, test } from "vitest"
+
+
+describe('Cliente routes', async () => {
+     const LoginResponse = await app.inject({
+          method: "POST",
+          url: `/login`,
+          payload: {
+               email: "exemplo@gmail.com",
+               senha: "exemplo123",
+          },
+     })
+
+     expect(LoginResponse.statusCode, "Cliente logado com sucesso").toBe(200)
+     expect(LoginResponse.headers["set-cookie"]).toBeDefined()
+
+     const cookieWithAuthorization = LoginResponse.headers["set-cookie"]
+     
+
+     test('GET /clientes', async () => {
+          const response = await app.inject({
+               method: "GET",
+               headers: {
+                    cookie: cookieWithAuthorization,
+               },
+               url: `/clientes`,
+          })
+
+          expect(response.statusCode).toBe(200)
+     })
+
+     test('GET /clientes/:slug', async () => { 
+          const slug = 'Jhonata'
+          const response = await app.inject({
+               method: "GET",
+               headers: {
+                    cookie: cookieWithAuthorization,
+               },
+
+               url: `/clientes/${slug}`,
+          })
+
+          expect(response.statusCode).toBe(200)
+     })
+})
