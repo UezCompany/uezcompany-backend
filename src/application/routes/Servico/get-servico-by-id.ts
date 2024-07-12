b51155571889383a5f1da88ca9e722b3e31a13ac
Danilo <htpps.sw@gmail.com>
2024-07-12 01:25:29 -0300

    docs: Add swagger to category routes

--- src/application/routes/Servico/get-servico-by-id.ts
+++ src/application/routes/Servico/get-servico-by-id.ts
@@ -1,15 +1,26 @@
 import { FastifyInstance } from "fastify"
 import { serviceRepository } from "@/repository/ServiceRepository"
 import { z } from "zod"
+import { ZodTypeProvider } from "fastify-type-provider-zod"
 
 export default async function GetServicoById(app: FastifyInstance) {
-  app.get("/servicos/:id", async (request, reply) => {
-    const params = z.object({
-      id: z.string().uuid(),
-    })
-    const { id } = params.parse(request.params)
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .get("/servicos/:id", {
+      schema: {
+        summary: "Get service by Id",
+        tags: ["Servico"],
+        params: z.object({
+          id: z.string().uuid(),
+        }),
+      }
+    }, async (request, reply) => {
+      const params = z.object({
+        id: z.string().uuid(),
+      })
+      const { id } = params.parse(request.params)
 
-    const servico = await serviceRepository.getServicesById(id)
-    return reply.status(200).send(servico || null)
-  })
+      const servico = await serviceRepository.getServicesById(id)
+      return reply.status(200).send(servico || null)
+    })
 }
--- src/application/routes/Servico/get-servicos.ts
+++ src/application/routes/Servico/get-servicos.ts
@@ -1,9 +1,17 @@
 import { FastifyInstance } from "fastify"
 import { serviceRepository } from "@/repository/ServiceRepository"
+import { ZodTypeProvider } from "fastify-type-provider-zod"
 
 export default async function GetServicos(app: FastifyInstance) {
-  app.get("/servicos", async (request, reply) => {
-    const servicos = await serviceRepository.getServices()
-    return reply.status(200).send(servicos)
-  })
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .get("/servicos", {
+      schema: {
+        summary: "Get all services",
+        tags: ["Servico"],
+      }
+    }, async (request, reply) => {
+      const servicos = await serviceRepository.getServices()
+      return reply.status(200).send(servicos)
+    })
 }
