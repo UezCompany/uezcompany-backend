1646383869cb6134d80cabb769a08891d87ac3a8
Danilo <htpps.sw@gmail.com>
2024-07-13 00:53:05 -0300

    docs: Add swagger to payment routes

--- src/application/routes/billing/create-payment-method.ts
+++ src/application/routes/billing/create-payment-method.ts
@@ -1,28 +1,40 @@
 import { stripe } from "@/infra/lib/stripe"
 import { FastifyInstance } from "fastify"
 import { z } from "zod"
+import { ZodTypeProvider } from "fastify-type-provider-zod"
 
 export default async function CreatePaymentMethod(app: FastifyInstance) {
-  app.post("/create/paymentMethod", async (request, reply) => {
-    const createPaymentMethodBody = z.object({
-      paymentMethodId: z.string(),
-      customerId: z.string(),
-    })
-    const { paymentMethodId, customerId } = createPaymentMethodBody.parse(
-      request.body,
-    )
-
-    // Aqui você pode adicionar a lógica para criar o método de pagamento no Stripe
-    try {
-      await stripe.paymentMethods.attach(paymentMethodId, {
-        customer: customerId,
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .post("/create/paymentMethod", {
+      schema: {
+        summary: "Create a new payment method",
+        tags: ["Payment"],
+        body: z.object({
+          paymentMethodId: z.string(),
+          customerId: z.string(),
+        })
+      },
+    }, async (request, reply) => {
+      const createPaymentMethodBody = z.object({
+        paymentMethodId: z.string(),
+        customerId: z.string(),
       })
-      reply
-        .status(200)
-        .send({ message: "Método de pagamento criado com sucesso" })
-    } catch (error) {
-      console.error("Error creating payment method:", error)
-      reply.status(500).send({ error: "Internal server error" })
-    }
-  })
+      const { paymentMethodId, customerId } = createPaymentMethodBody.parse(
+        request.body,
+      )
+
+      // Aqui você pode adicionar a lógica para criar o método de pagamento no Stripe
+      try {
+        await stripe.paymentMethods.attach(paymentMethodId, {
+          customer: customerId,
+        })
+        reply
+          .status(200)
+          .send({ message: "Método de pagamento criado com sucesso" })
+      } catch (error) {
+        console.error("Error creating payment method:", error)
+        reply.status(500).send({ error: "Internal server error" })
+      }
+    })
 }
